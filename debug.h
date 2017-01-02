a0388aa5d807709b1f1f4e8ae4a826701f369871
Carlo Wood <carlo@alinoe.com>
2017-01-02 02:44:19 +0100

    End of WIP. Make everything work again.

--- debug.h
+++ debug.h
@@ -1,4 +1,6 @@
 #pragma once
 
 #define NAMESPACE_DEBUG myproject::debug
+#define NAMESPACE_DEBUG_START namespace myproject { namespace debug {
+#define NAMESPACE_DEBUG_END } }
 #include "cwd/debug.h"
